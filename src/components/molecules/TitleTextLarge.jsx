e22038aa69abe9e9d8c145b5f94bf8d9e8768aca
zenabii <maria.turtles@outlook.com>
2022-09-21 14:11:03 +0200

    large title text

---
+++ src/components/molecules/TitleTextLarge.jsx
@@ -0,0 +1,20 @@
+import { Paragraph } from "../atoms/Paragraph";
+import { Button } from "../atoms/Button";
+
+export const TitleTextLarge = ({
+    subject,
+    title,
+    paragraph,
+    theme,
+    secondParagraph,
+}) => (
+    <div className={theme === "blue" ? "text-part-blue" : "text-part-white"}>
+        <p className="subject">{subject}</p>
+        <h2>{title}</h2>
+        <div className="paragraphColumn">
+            <Paragraph paragraph={paragraph} />
+            <Paragraph paragraph={secondParagraph} />
+            <Button theme="black" link="#" text="Send an enquiry" />
+        </div>
+    </div>
+);
--- src/components/templates/ActivitySection.jsx
+++ src/components/templates/ActivitySection.jsx
@@ -1,14 +1,19 @@
-import { TitleText } from "../molecules/TitleText";
+import { TitleTextLarge } from "../molecules/TitleTextLarge";
 
 const image = require("../../images/atlantic-puffin-grass-green.jpg");
 
 export const ActivitySection = () => (
     <section className="two-columns-grid">
-        <TitleText
+        <TitleTextLarge
             theme="blue"
             subject="introduction"
             title="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            faucib us massa at auctor lacinia. Proin porta a orci in aliquet.
+            Nam a nunc quam. Maecenas lacinia facilisis bibendum. Suspendisse
+            potenti. Vivamus erat ligula, iaculis quis pulvinar id, imperdiet et
+            orci."
+            secondParagraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
             faucib us massa at auctor lacinia. Proin porta a orci in aliquet.
             Nam a nunc quam. Maecenas lacinia facilisis bibendum. Suspendisse
             potenti. Vivamus erat ligula, iaculis quis pulvinar id, imperdiet et
